cc0e7311746a5eb48e520d531fa2cb2190a5d48e
drowning <drowning@DESKTOP-4QRPJV8>
2024-03-29 00:08:58 +0300

    The endpoing of the game have been added of the game process, and the first documentation for objects has also been added

--- ChessLogic/Moves/Move.cs
+++ ChessLogic/Moves/Move.cs
@@ -1,11 +1,23 @@
 ﻿namespace ChessLogic
 {
+    /// <summary>
+    /// Класс, олицетворяющий ход
+    /// </summary>
     public abstract class Move
     {
         public abstract MoveType Type { get; }
         public abstract Position FromPosition { get; }
         public abstract Position ToPosition { get; }
+        /// <summary>
+        /// Выполнить текущий ход
+        /// </summary>
+        /// <param name="board">Доска, на которой собирается произойти ход</param>
         public abstract void Execute(Board board);
+        /// <summary>
+        /// Проверка легальности хода. Если после данного хода оппонент имеет шах на нас, то ход невозможен
+        /// </summary>
+        /// <param name="board">Доска для проверки, где происходит ход</param>
+        /// <returns>true, если ход возможен, false, если мы походим и на нашего короля будет атака</returns>
         public virtual bool IsLegal(Board board)
         {
             Player color = board[FromPosition].Color;
